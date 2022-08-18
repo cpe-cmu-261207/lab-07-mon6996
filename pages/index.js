d501824fae3243c069f7cdfba07de47805439d6d
M <thanaporn_chan@cmu.ac.th>
2022-08-18 15:40:38 +0700

    completed

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,114 @@
 import { useEffect, useState } from "react";
 import Todo from "../components/Todo";
 import {
-  IconCheck,
-  IconTrash,
-  IconArrowUp,
-  IconArrowDown,
+	IconCheck,
+	IconTrash,
+	IconArrowUp,
+	IconArrowDown,
+	IconTemperature,
 } from "@tabler/icons";
 
 export default function Home() {
-  const deleteTodo = (idx) => {};
+	const [todoInput, setTodoInput] = useState('');
+	const [todos, setTodos] = useState([]);
 
-  const markTodo = (idx) => {};
+	useEffect(() => {
+		const todoStr = localStorage.getItem('react-todos');
+		if (!todoStr) setTodos([]);
+		else setTodos(JSON.parse(todoStr));
+	}, []);
 
-  const moveUp = (idx) => {};
+	const [isFirstRender, setIsFirstRender] = useState(true);
 
-  const moveDown = (idx) => {};
+	useEffect(() => {
+		if (isFirstRender) {
+			setIsFirstRender(false);
+			return;
+		}
+		saveTodos();
+	}, [todos]);
 
-  return (
-    <div>
-      {/* Entire App container (required for centering) */}
-      <div style={{ maxWidth: "700px" }} className="mx-auto">
-        {/* App header */}
-        <p className="display-4 text-center fst-italic m-4">
-          Minimal Todo List <span className="fst-normal">☑️</span>
-        </p>
-        {/* Input */}
-        <input
-          className="form-control mb-1 fs-4"
-          placeholder="insert todo here..."
-        />
-        {/* Todos */}
-        {/* Example 1 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo</span>
-        </div>
-        {/* Example 2 */}
-        <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
-          <span className="me-auto">Todo with buttons</span>
+	const onKeyUpHandler = (event) => {
+		if (event.key !== "Enter") return;
+		if (todoInput === '') alert("Todo cannot be empty");
+		else {
+			setTodos([{ title: todoInput, completed: false }, ...todos]);
+			setTodoInput('');
+		}
+	};
 
-          <button className="btn btn-success">
-            <IconCheck />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowUp />
-          </button>
-          <button className="btn btn-secondary">
-            <IconArrowDown />
-          </button>
-          <button className="btn btn-danger">
-            <IconTrash />
-          </button>
-        </div>
+	const deleteTodo = (idx) => {
+		todos.splice(idx, 1);
+		setTodos([...todos]);
+	};
 
-        {/* summary section */}
-        <p className="text-center fs-4">
-          <span className="text-primary">All (2) </span>
-          <span className="text-warning">Pending (2) </span>
-          <span className="text-success">Completed (0)</span>
-        </p>
+	const markTodo = (idx) => {
+		todos[idx].completed = !todos[idx].completed;
+		setTodos([...todos]);
+	};
 
-        {/* Made by section */}
-        <p className="text-center mt-3 text-muted fst-italic">
-          made by Chayanin Suatap 12345679
-        </p>
-      </div>
-    </div>
-  );
+	const moveUp = (idx) => {
+		if (idx === 0) return;
+		const temp = todos[idx];
+		todos[idx] = todos[idx - 1];
+		todos[idx - 1] = temp;
+		setTodos([...todos]);
+	};
+
+	const moveDown = (idx) => {
+		if (idx === todos.length - 1) return;
+		const temp = todos[idx];
+		todos[idx] = todos[idx + 1];
+		todos[idx + 1] = temp;
+		setTodos([...todos]);
+	};
+
+	const saveTodos = () => {
+		const todosStr = JSON.stringify(todos);
+		localStorage.setItem('react-todos', todosStr);
+	};
+
+	return (
+		<div>
+			{/* Entire App container (required for centering) */}
+			<div style={{ maxWidth: "700px" }} className="mx-auto">
+				{/* App header */}
+				<p className="display-4 text-center fst-italic m-4">
+					Minimal Todo List <span className="fst-normal">☑️</span>
+				</p>
+				{/* Input */}
+				<input
+					className="form-control mb-1 fs-4"
+					placeholder="insert todo here..."
+					onChange={event => setTodoInput(event.target.value)}
+					value={todoInput}
+					onKeyUp={onKeyUpHandler}
+				/>
+
+				{
+					todos.map((todo, i) => <Todo
+						title={todo.title}
+						completed={todo.completed}
+						key={i}
+						onDelete={() => deleteTodo(i)}
+						onMark={() => markTodo(i)}
+						onUp={() => moveUp(i)}
+						onDown={() => moveDown(i)}
+					/>)
+				}
+
+				{/* summary section */}
+				<p className="text-center fs-4">
+					<span className="text-primary">All ({todos.length}) </span>
+					<span className="text-warning">Pending ({todos.filter((x) => x.completed === false).length}) </span>
+					<span className="text-success">Completed ({todos.filter((x) => x.completed === true).length})</span>
+				</p>
+
+				{/* Made by section */}
+				<p className="text-center mt-3 text-muted fst-italic">
+					made by Thanaporn Chanchanayothin 640610638
+				</p>
+			</div>
+		</div>
+	);
 }
